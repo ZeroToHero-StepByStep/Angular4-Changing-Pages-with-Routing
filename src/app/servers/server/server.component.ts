7cc58babc033d05f3d377cb770126486b029c278
Pei <jzha471@aucklanduni.ac.nz>
2017-10-15 10:00:33 +1300

    126. Practicing and Common Gothas

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { ServersService } from '../servers.service';
-import {Router} from '@angular/router';
+import {ServersService} from '../servers.service';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -9,14 +9,18 @@ import {Router} from '@angular/router';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number, name: string, status: string };
 
-  constructor(private serversService: ServersService )  { }
-
-  ngOnInit() {
-    this.server = this.serversService.getServer(1);
+  constructor(private serversService: ServersService, private route: ActivatedRoute) {
   }
 
+  ngOnInit() {
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    this.route.params.subscribe((params: Params) =>{
+      this.server = this.serversService.getServer(+params['id']) ;
+    });
+  }
 
 
 }
