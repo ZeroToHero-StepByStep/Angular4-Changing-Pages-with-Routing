4bff6164764c8c366bcfbbe774f2ccaf31047824
Pei <jzha471@aucklanduni.ac.nz>
2017-10-15 16:30:41 +1300

    132. Outsourcing the Route Configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,21 +13,8 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import {RouterModule, Routes} from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {AppRoutingMoudle} from './users/app-routing.moudle';
 
-const appRoutes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'users', component: UsersComponent, children:[
-    {path: ':id/:name', component: UserComponent},
-  ]},
-
-  {path: 'servers', component: ServersComponent, children:[
-    {path: ':id', component: ServerComponent},
-    {path: ':id/edit', component: EditServerComponent}
-  ]},
-  {path: 'not-found', component: PageNotFoundComponent},
-  {path: '**', redirectTo: '/not-found'}
-
-];
 @NgModule({
   declarations: [
     AppComponent,
@@ -42,8 +29,8 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule ,
-    RouterModule.forRoot(appRoutes)
+    HttpModule,
+    AppRoutingMoudle
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
